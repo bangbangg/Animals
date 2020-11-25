44f7a9050e4042bc2cc3c4d76fcdd12e666c82e4
Baium <42148640+bangbangg@users.noreply.github.com>
2020-11-25 16:06:38 +0300

    Update index.jsx
    Теперь  работает без расширения redux-devtoolz в браузере не выдавая ошибки

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -12,9 +12,9 @@ import './Styles/style.scss';
 
 const persistedState = loadState();
 
-const store = createStore(rootReducer, persistedState, compose(applyMiddleware(thunk),
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-));
+);
 
 store.subscribe(() => {
   saveState(store.getState());
